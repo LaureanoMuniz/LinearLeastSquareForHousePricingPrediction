629316ca5a4bb976c7ea8916e94528feb1175512
Daniel Wappner <danielituswapp@gmail.com>
2021-06-30 16:31:55 -0300

    experimento 3

--- experiments/experiment3.py
+++ experiments/experiment3.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
-#from . import nombres
-#from experiments import filter
-#from experiments import geo
+from experiments.nombres import LATITUD,LONGITUD,TIPO_DE_PROPIEDAD
+import experiments.filter as filter
+import experiments.geo as geo
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -12,90 +12,32 @@ import math
 # import build.metnum as metnum
 import sklearn.linear_model as metnum
 
-###import a mano
-
-def distance(lat1, lon1, lat2, lon2):
-    p = math.pi/180
-    a = 0.5 - math.cos((lat2-lat1)*p)/2 + math.cos(lat1*p) *\
-        math.cos(lat2*p) * (1-math.cos((lon2-lon1)*p))/2
-
-    if a < 0:
-        a = 0
-    if math.sqrt(a) > 1 or math.sqrt(a) < 0:
-        print(a)
-
-    a = min(1, math.sqrt(a))
-    a = max(0, a)
-    return 12742 * math.asin(a)
-
-LATITUD = "lat"
-LONGITUD = "lng"
-METROS_TOT = "metrostotales"
-METROS_CUB = "metroscubiertos"
-PRECIO = "precio"
-HABITACIONES = "habitaciones"
-BAÑOS = "banos"
-GARAJES = "garages"
-TÍTULO = "titulo"
-DESCRIPCIÓN = "descripcion"
-GIMNASIO = "gimnasio"
-USOS_MULT = "usosmultiples"
-PILETA = "piscina"
-ESCUELAS = "escuelascercanas"
-COMERCIOS = "centroscomercialescercanos"
-SEGURIDAD = "seguridad"
-SALAS = "salas"
-NATURALEZA = "naturaleza"
-CHETOS = "chetos"
-PRECIO_M2 = "precio_por_m2"
-PRECIO_M2_NEAREST = "precio_por_m2_nearest"
-TIPO_DE_PROPIEDAD = "tipodepropiedad"
-ONE = "one"
-eps = 1e-6
-
-def filter_city(df, ciudad):
-    in_city = (df[LATITUD] > ciudad.lat_lo)\
-        & (df[LATITUD] < ciudad.lat_hi)\
-        & (df[LONGITUD] > ciudad.lng_lo)\
-        & (df[LONGITUD] < ciudad.lng_hi)
-
-    return df[in_city]
-
-def filter(df):
-    useful = df[LONGITUD].notnull() & df[LONGITUD].abs() > eps
-    useful &= df[LATITUD].notnull() & df[LATITUD].abs() > eps
-    useful &= df[METROS_TOT].notnull() & df[METROS_TOT].abs() > eps
-    useful &= df[METROS_CUB].notnull() & df[METROS_CUB].abs() > eps
-    useful &= df[HABITACIONES].notnull() & df[HABITACIONES].abs() > eps
-    useful &= df[PRECIO].notnull() & df[PRECIO].abs() > eps
-    useful &= df[BAÑOS].notnull()
-    useful &= df[GARAJES].notnull()
-    useful &= df[TÍTULO].notnull()
-    useful &= df[GIMNASIO].notnull()
-    useful &= df[USOS_MULT].notnull()
-    useful &= df[PILETA].notnull()
-    useful &= df[ESCUELAS].notnull()
-    useful &= df[COMERCIOS].notnull()
-    useful &= df[DESCRIPCIÓN].notnull()
-    return df[useful]
-
-df = pd.read_csv('data/train.csv')
-df = df.sample(frac=1)
-df = filter(df)
-
-###fin del import a mano
-
 
 def turn_lat_long_into_XY(df,ciudad):
 	def info(row):
-		x = distance(ciudad.lat_lo,ciudad.lng_lo,ciudad.lat_lo,row[LONGITUD])
-		y = distance(ciudad.lat_lo,ciudad.lng_lo,row[LATITUD],ciudad.lng_lo)
+		x = geo.distance(ciudad.lat_lo,ciudad.lng_lo,ciudad.lat_lo,row[LONGITUD])
+		y = geo.distance(ciudad.lat_lo,ciudad.lng_lo,row[LATITUD],ciudad.lng_lo)
 		row['X'] = x
 		row['Y'] = y
 		return row
 	return df.apply(info, axis=1) 
 
+def turn_propiedad_into_bin(df):
+	def info(row):
+		propiedadbin = row[TIPO_DE_PROPIEDAD] == "Casa"
+		row['propiedadbin'] = int(propiedadbin)
+		return row
+	return df.apply(info, axis=1)
 
+def matriz_de_gaussianas_aplicadas(df,Gaussianas):
+	out = np.zeros((len(df),cantidad_Gaussianas))
+	for i,funcion in enumerate(Gaussianas):
+		#try:
+			out[:,i] = funcion.apply_vectorized(df['X'].tolist(),df['Y'].tolist())
+		#except:
+			#print("error, iteration is:")
+			#print(i)
+	return out
 
 @dataclass
 class Ciudad:
@@ -158,8 +100,8 @@ cantidad_Gaussianas = 25
 
 
 def para_ciudad(ciudad, df):
-	distx = distance(ciudad.lat_lo,ciudad.lng_lo,ciudad.lat_hi,ciudad.lng_lo)
-	disty = distance(ciudad.lat_lo,ciudad.lng_lo,ciudad.lat_lo,ciudad.lng_hi)
+	distx = geo.distance(ciudad.lat_lo,ciudad.lng_lo,ciudad.lat_hi,ciudad.lng_lo)
+	disty = geo.distance(ciudad.lat_lo,ciudad.lng_lo,ciudad.lat_lo,ciudad.lng_hi)
 	#nos movemos en el plano [0,distx]; [0,dist_y]. No estoy seguro si hacia falta
 	tamano_x_cuadrado = distx/cantidad_Gaussianas
 	tamano_y_cuadrado = disty/cantidad_Gaussianas
@@ -175,21 +117,30 @@ def para_ciudad(ciudad, df):
 		mu_y = tamano_y_cuadrado * (i + 1/2)
 		Gaussianas.append(Gaussiana(mu_x,mu_y,sigma_x,sigma_y))
 	
-	df = filter_city(df, ciudad)
+	df = filter.filter_city(df, ciudad)
 	df = df[[LATITUD,LONGITUD,TIPO_DE_PROPIEDAD]]
 	df = turn_lat_long_into_XY(df,ciudad)
+	df = turn_propiedad_into_bin(df)
 
 	n = len(df)
 	train = df[:n*8//10]
 	test = df[n*8//10:]
 
-	mean_squares_train = np.zeros((len(train),cantidad_Gaussianas))
-	for i,funcion in enumerate(Gaussianas):
-		#try:
-			mean_squares_train[:,i] = funcion.apply_vectorized(train['X'].tolist(),train['Y'].tolist())
+	mean_squares_train = matriz_de_gaussianas_aplicadas(train,Gaussianas)
+	mean_squares_test = matriz_de_gaussianas_aplicadas(test,Gaussianas)
 
-		#except:
-			#print("error, iteration is:")
-			#print(i)
+	regressor = metnum.LinearRegression()
+	regressor.fit(mean_squares_train,train['propiedadbin'])
+	predicted = regressor.predict(mean_squares_test)
 
-para_ciudad(ciudades[0],df)
+	mean_predictor = np.full(len(test),np.mean(test['propiedadbin'].to_numpy()))
+	error = predicted - test['propiedadbin'].to_numpy()
+	error_de_la_media = mean_predictor - test['propiedadbin'].to_numpy()
+	error = math.sqrt((error**2).sum() / len(test))
+	error_de_la_media = math.sqrt((error_de_la_media**2).sum() / len(test)) 
+	print(1 - (error / error_de_la_media))
+
+
+def experimento3(df):
+    for ciudad in ciudades:
+        para_ciudad(ciudad, df)
