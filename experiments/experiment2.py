6e2b4867805a50a14f6ab0cc1e5603deb050f104
Carlos Miguel Soto <miguelsotocarlos@gmail.com>
2021-07-03 19:58:00 -0300

    k nearest

--- experiments/experiment2.py
+++ experiments/experiment2.py
@@ -3,15 +3,23 @@ import experiments.nombres as nombres
 
 
 def add_knearest(train, test):
+    test = test.copy()
+    # print(len(train))
+    # train = train.sample(100)
+
     def info(row):
         suma = 0
         weight = 0
-        for t in train.iterrows():
+        for i, t in train.iterrows():
+            # print(i)
             d = distance(
                 t[nombres.LATITUD], t[nombres.LONGITUD],
                 row[nombres.LATITUD], row[nombres.LONGITUD])
             w = 0 if d < nombres.eps else 1/d
             weight += w
             suma += w * t[nombres.PRECIO_M2]
-        row[nombres.PRECIO_M2_NEAREST] = suma / weight
-    return test.apply(info, axis=1)
+        # print("row done")
+        return suma / weight
+    res = test.apply(info, axis=1)
+    test[nombres.PRECIO_M2_NEAREST] = res
+    return test
--- experiments/experimento1.py
+++ experiments/experimento1.py
@@ -14,11 +14,13 @@ from experiments.kfold import kfold
 from experiments.cities import ciudades
 
 
-def para_ciudad(ciudad, df, con_knear=False):
+def para_ciudad(ciudad, df, tipo, con_knear=False):
     df = filter_city(df, ciudad)
-    df = df[df[nombres.TIPO_DE_PROPIEDAD] == 'Casa']
-
+    df = df[df[nombres.TIPO_DE_PROPIEDAD] == tipo]
+    title = f"KNearest: {con_knear}, {ciudad.nombre}, {tipo}"
+    print(title)
     errors = []
+    metrics = np.zeros((0, 3))
     for train, test in kfold(df):
         if con_knear:
             train = add_knearest(train, train)
@@ -54,17 +56,24 @@ def para_ciudad(ciudad, df, con_knear=False):
 
         predicted = regressor.predict(test[features])[:, 0]
         actual = test[nombres.PRECIO].values
-        for i in range(len(predicted)):
-            print(predicted[i], test.iloc[i][nombres.PRECIO])
-        print(len(predicted))
+        # for i in range(len(predicted)):
+        # print(predicted[i], test.iloc[i][nombres.PRECIO])
+        # print(len(predicted))
 
-        print(métricas.rmse(actual, predicted))
         errors = np.append(errors, actual-predicted)
+        metrics = np.append(metrics, np.array([[
+            métricas.rmse(actual, predicted),
+            métricas.rmsle(actual, predicted),
+            métricas.r2(actual, predicted),
+        ]]), axis=0)
         # print(métricas.rmsle(actual, predicted))
 
     plt.figure()
+    print(metrics)
+    print(metrics.mean(axis=0))
+    plt.title(title)
     plt.hist(errors, bins=100)
-    plt.show()
+    plt.savefig(f"images/{title}.png")
 
     # plt.figure()
     # plt.scatter(x=df[LONGITUD], y=df[LATITUD])
@@ -81,5 +90,10 @@ def para_ciudad(ciudad, df, con_knear=False):
 
 
 def experimento1(df, con_knear=False):
-    for ciudad in ciudades:
-        para_ciudad(ciudad, df)
+    df = df.sample(frac=0.01)
+    tipos = ['Casa', 'Apartamento']
+    if con_knear:
+        tipos = ['Casa']
+    for tipo in tipos:
+        for ciudad in ciudades:
+            para_ciudad(ciudad, df, tipo, con_knear=con_knear)
